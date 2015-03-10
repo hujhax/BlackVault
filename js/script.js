771ac438e1b29837c1212458cae1cb8b13b2a27c
Peter Rogers <peter.rogers.consulting@gmail.com>
2015-03-09 21:18:32 -0500

    Making the "hover" jQuery code use selectors properly.

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(function() {
 
 var hoverLogic = function(topbar, target, biobox) {
   $(topbar).on('mouseenter', target, function(e){ 
-    $(biobox).css('display','block');
+    $(this).siblings(biobox).css('display','block');
   });
 
   $('body').on('mouseleave', biobox, function(e){
@@ -23,21 +23,4 @@ var hoverLogic = function(topbar, target, biobox) {
   });
 }
 
-hoverLogic("#bradstopbar", "#biotarget2", "#author-bio-box");
-hoverLogic("#emilystopbar", "#biotarget3", "#emilysauthor-bio-box");
-hoverLogic("#andreastopbar", "#biotarget1", "#andreasauthor-bio-box");
-hoverLogic("#jaystopbar", "#biotarget4", "#jaysauthor-bio-box");
-hoverLogic("#jaymestopbar", "#biotarget5", "#jaymesauthor-bio-box");
-hoverLogic("#jessicastopbar", "#biotarget6", "#jessicasauthor-bio-box");
-hoverLogic("#justinstopbar", "#biotarget7", "#justinsauthor-bio-box");
-hoverLogic("#marcstopbar", "#biotarget8", "#marcsauthor-bio-box");
-hoverLogic("#marcstopbar", "#biotarget8", "#marcsauthor-bio-box");
-hoverLogic("#paulstopbar", "#biotarget9", "#paulsauthor-bio-box");
-hoverLogic("#peterstopbar", "#biotarget10", "#petersauthor-bio-box");
-hoverLogic("#roystopbar", "#biotarget11", "#roysauthor-bio-box");
-hoverLogic("#brentstopbar", "#biotarget13", "#brentsauthor-bio-box");
-hoverLogic("#claystopbar", "#biotarget14", "#claysauthor-bio-box");
-hoverLogic("#lindseystopbar", "#biotarget15", "#lindseysauthor-bio-box");
-hoverLogic("#cindystopbar", "#biotarget16", "#cindysauthor-bio-box");
-hoverLogic("#michaelstopbar", "#biotarget17", "#michaelsauthor-bio-box");
-hoverLogic("#natestopbar", "#biotarget18", "#natesauthor-bio-box");
+hoverLogic(".bio-thumbnail", ".authoravi", ".bio-full");
