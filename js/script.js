d82730fdc3d2e3f52caf6df9dc25b06a1d96b743
Peter Rogers <peter.rogers.consulting@gmail.com>
2015-03-09 20:54:22 -0500

    Removing duplicate functions. http://codethisnotthat.tumblr.com/post/111534971034/dont-copy-code

--- js/script.js
+++ js/script.js
@@ -13,201 +13,31 @@ $(function() {
     });
 });
 
-// brads bio hover logic
-$(function() {
-    $('#bradstopbar').on('mouseenter', '#biotarget2', function(e) {
-        $('#author-bio-box').css('display', 'block');
-    });
+var hoverLogic = function(topbar, target, biobox) {
+  $(topbar).on('mouseenter', target, function(e){ 
+    $(biobox).css('display','block');
+  });
 
-    $('body').on('mouseleave', '#author-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
+  $('body').on('mouseleave', biobox, function(e){
+    $(this).css('display','none');
+  });
+}
 
-// emilys bio hover logic
-$(function() {
-    $('#emilystopbar').on('mouseenter', '#biotarget3', function(e) {
-        $('#emilysauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#emilysauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// andreas' bio hover logic
-$(function() {
-    $('#andreastopbar').on('mouseenter', '#biotarget1', function(e) {
-        $('#andreasauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#andreasauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// jays bio hover logic
-$(function() {
-    $('#jaystopbar').on('mouseenter', '#biotarget4', function(e) {
-        $('#jaysauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#jaysauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// jaymes bio hover logic
-$(function() {
-    $('#jaymestopbar').on('mouseenter', '#biotarget5', function(e) {
-        $('#jaymesauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#jaymesauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// jessicas bio hover logic
-$(function() {
-    $('#jessicastopbar').on('mouseenter', '#biotarget6', function(e) {
-        $('#jessicasauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#jessicasauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// justins bio hover logic
-$(function() {
-    $('#justinstopbar').on('mouseenter', '#biotarget7', function(e) {
-        $('#justinsauthor-bio-box').css('display', 'block');
-    });
-
-    $('#justinstopbar').on('mouseleave', '#justinsauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// marcs bio hover logic
-$(function() {
-    $('#marcstopbar').on('mouseenter', '#biotarget8', function(e) {
-        $('#marcsauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#marcsauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// pauls bio hover logic
-$(function() {
-    $('#paulstopbar').on('mouseenter', '#biotarget9', function(e) {
-        $('#paulsauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#paulsauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// peters bio hover logic
-$(function() {
-    $('#peterstopbar').on('mouseenter', '#biotarget10', function(e) {
-        $('#petersauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#petersauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// roys bio hover logic
-$(function() {
-    $('#roystopbar').on('mouseenter', '#biotarget11', function(e) {
-        $('#roysauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#roysauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// ryans bio hover logic
-$(function() {
-    $('#ryanstopbar').on('mouseenter', '#biotarget12', function(e) {
-        $('#ryansauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#ryansauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// brents bio hover logic
-$(function() {
-    $('#brentstopbar').on('mouseenter', '#biotarget13', function(e) {
-        $('#brentsauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#brentsauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// clays bio hover logic
-$(function() {
-    $('#claystopbar').on('mouseenter', '#biotarget14', function(e) {
-        $('#claysauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#claysauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-
-// lindseys bio hover logic
-$(function() {
-    $('#lindseystopbar').on('mouseenter', '#biotarget15', function(e) {
-        $('#lindseysauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#lindseysauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// cindys bio hover logic
-$(function() {
-    $('#cindystopbar').on('mouseenter', '#biotarget16', function(e) {
-        $('#cindysauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#cindysauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// michaels bio hover logic
-$(function() {
-    $('#michaelstopbar').on('mouseenter', '#biotarget17', function(e) {
-        $('#michaelsauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#michaelsauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
-
-// nates bio hover logic
-$(function() {
-    $('#natestopbar').on('mouseenter', '#biotarget18', function(e) {
-        $('#natesauthor-bio-box').css('display', 'block');
-    });
-
-    $('body').on('mouseleave', '#natesauthor-bio-box', function(e) {
-        $(this).css('display', 'none');
-    });
-});
+hoverLogic("#bradstopbar", "#biotarget2", "#author-bio-box");
+hoverLogic("#emilystopbar", "#biotarget3", "#emilysauthor-bio-box");
+hoverLogic("#andreastopbar", "#biotarget1", "#andreasauthor-bio-box");
+hoverLogic("#jaystopbar", "#biotarget4", "#jaysauthor-bio-box");
+hoverLogic("#jaymestopbar", "#biotarget5", "#jaymesauthor-bio-box");
+hoverLogic("#jessicastopbar", "#biotarget6", "#jessicasauthor-bio-box");
+hoverLogic("#justinstopbar", "#biotarget7", "#justinsauthor-bio-box");
+hoverLogic("#marcstopbar", "#biotarget8", "#marcsauthor-bio-box");
+hoverLogic("#marcstopbar", "#biotarget8", "#marcsauthor-bio-box");
+hoverLogic("#paulstopbar", "#biotarget9", "#paulsauthor-bio-box");
+hoverLogic("#peterstopbar", "#biotarget10", "#petersauthor-bio-box");
+hoverLogic("#roystopbar", "#biotarget11", "#roysauthor-bio-box");
+hoverLogic("#brentstopbar", "#biotarget13", "#brentsauthor-bio-box");
+hoverLogic("#claystopbar", "#biotarget14", "#claysauthor-bio-box");
+hoverLogic("#lindseystopbar", "#biotarget15", "#lindseysauthor-bio-box");
+hoverLogic("#cindystopbar", "#biotarget16", "#cindysauthor-bio-box");
+hoverLogic("#michaelstopbar", "#biotarget17", "#michaelsauthor-bio-box");
+hoverLogic("#natestopbar", "#biotarget18", "#natesauthor-bio-box");
